7e2710285ee7c1d6cdec7efe32a062a9cacb1aa6
MariiaSizova <sizovamaria24@gmail.com>
2023-04-25 11:14:36 +0300

    adding levels, starting to add a main page

--- src/App.jsx
+++ src/App.jsx
@@ -39,7 +39,7 @@ class App extends Component {
       audioEnd: new Audio(gameEnd),
       audioStart: new Audio(gameStart),
       audioClick: new Audio(clickBtn),
-      finalText: ""
+      finalText: "",
     });
   }
 
@@ -66,28 +66,27 @@ class App extends Component {
   }
 
   handleCircle = (circle) => {
-    this.setState({countClicks: this.state.countClicks + 1})
+    this.setState({countClicks: this.state.countClicks + 1});
     this.state.audioClick.play();
     
     if(this.state.activeNumber === circle) {
-      !this.state.clicked && this.setState({score: this.state.score + 1, clicked: true})
+      !this.state.clicked && this.setState({score: this.state.score + 1, clicked: true});
     } else if((this.state.activeNumber !== circle)) {
       this.setState({wrongCircle: circle});
       this.setState({wrongCircleCount: this.state.wrongCircleCount + 1, clicked: true});
       if (this.state.wrongCircleCount >=2) {
-        this.handleStopGame()
+        this.handleStopGame();
       }
     }
-
-
   }
 
   getRndInt = (min, max) => Math.floor(Math.random() * (max - min + 1)) + min;
 
   randomNumber = () => {
     const missedRounds = (this.state.countRounds - this.state.countClicks);
+/*     this.handleLifes(missedRounds); */
     if(missedRounds <= 3){
-      this.setState({countRounds: this.state.countRounds+1})
+      this.setState({countRounds: this.state.countRounds+1});
       this.handleLevelGame();
       this.setState({wrongCircle: null}); 
       let nextActive;
@@ -98,9 +97,22 @@ class App extends Component {
       });
       this.changingActiveCircle();
     } else {
-      this.handleStopGame()
+      this.handleStopGame();
     }
   };
+/* 
+  handleLifes = (missedRounds) => {
+    
+    if (missedRounds === 2) {
+      this.setState({color: "#2bc8401a"})
+    }
+    else if (missedRounds === 3) {
+      this.setState({color: "#febc2e1a"})
+    }
+    else if (missedRounds === 4) {
+      this.setState({color: "#fe5f581a"})
+    }
+  } */
 
   changingActiveCircle = () => {
     this.setState({clicked: false});
@@ -119,39 +131,48 @@ class App extends Component {
     else if(score >=4 && score < 8) {
       clearInterval(this.interval);
       this.interval = setInterval(this.randomNumber, 2500);
-      this.setState({finalText: "You need to learn A LOT how to catch up with bugs!"})
+      this.setState({finalText: "You need to learn A LOT how to catch up with bugs!"});
     }
     else if(score >=8 && score < 15) {
       clearInterval(this.interval);
       this.interval = setInterval(this.randomNumber, 2000);
-      this.setState({finalText: "You need to learn a little bit how to catch up with bugs!"})
+      this.setState({finalText: "You need to learn a little bit how to catch up with bugs!"});
     }
     else if(score >=15 && score < 30) {
       clearInterval(this.interval);
       this.interval = setInterval(this.randomNumber, 1000);
-      this.setState({finalText: "Wow... A few lessons more and you will be perfect!"})
+      this.setState({finalText: "Wow... A few lessons more and you will be perfect!"});
     }
     else if(score >=30 && score < 50) {
       clearInterval(this.interval);
       this.interval = setInterval(this.randomNumber, 800);
-      this.setState({finalText: "Where did you train this? Nice job!"})
+      this.setState({finalText: "Where did you train this? Nice job!"});
     }
     else if(score >=50) {
       clearInterval(this.interval);
       this.interval = setInterval(this.randomNumber, 500);
-      this.setState({finalText: "You are a master! Perfect!"})
+      this.setState({finalText: "You are a master! Perfect!"});
     }
   };
 
   handleStopGame = () => {
-    this.setState({finishGame: true})
+    this.setState({finishGame: true});
     this.state.audioEnd.play();
     clearInterval(this.interval);
   }
 
   handleCloseOverlay = () => {
-    this.setState({finishGame: false})
+    this.setState({finishGame: false});
     this.initializeState();
+  }
+
+  //firstPage
+
+  handleInput = (e) => {
+    e.preventDefault();
+    console.log(e.target.name);
+    console.log(e.target.value)
+    /* this.setState({[e.target.name]: e.target.value}); */
   }
   
   render() {
@@ -159,29 +180,53 @@ class App extends Component {
       <div className={classes.gamePage}>
         <div className={classes.gameCard}>
           <h1 className={classes.gameName}>Speed Game</h1>
+          <div className={classes.lives}>
+            <span className={`material-symbols-outlined ${classes.bug_1} ${this.state.wrongCircleCount>=3 ? classes.bug_1_lose  : ""}`}> bug_report </span>
+            <span className={`material-symbols-outlined ${classes.bug_2} ${this.state.wrongCircleCount>=2 ? classes.bug_2_lose  : ""}`}> bug_report </span>
+            <span className={`material-symbols-outlined ${classes.bug_3} ${this.state.wrongCircleCount>=1 && classes.bug_3_lose}`}> bug_report </span>
+          </div>
           <h2>Score {this.state.score}</h2>
-            <div className={classes.buttonsCircle}>
-              {this.state.circles.map((circle) => <Circle 
-                key={circle}
-                start={this.state.startGame}
-                active={circle === this.state.activeNumber} 
-                wrongCircle={circle === this.state.wrongCircle}
-                handleCircle={() => this.handleCircle(circle)} 
-                clicked={this.state.clicked}
-              />)}
-            </div>
-            {
-              (this.state.startGame) ? <button className={classes.button_start} onClick={this.handleStartButton}>Start</button>
-              : <button className={classes.button_start} onClick={this.handleStartButton}>End</button>
-            }
-            {
-              this.state.finishGame && <GameOver score={this.state.score} gameOver={this.state.finishGame} handleCloseOverlay={this.handleCloseOverlay} finalText={this.state.finalText}/>
-            }
-            
+          <div className={classes.buttonsCircle}>
+            {this.state.circles.map((circle) => <Circle 
+              key={circle}
+              start={this.state.startGame}
+              active={circle === this.state.activeNumber} 
+              wrongCircle={circle === this.state.wrongCircle}
+              handleCircle={() => this.handleCircle(circle)} 
+              clicked={this.state.clicked}
+            />)}
+          </div>
+          {
+            (this.state.startGame) ? <button className={classes.button_start} onClick={this.handleStartButton}>Start</button>
+            : <button className={classes.button_start} onClick={this.handleStartButton}>End</button>
+          }
+          {
+            this.state.finishGame && <GameOver score={this.state.score} gameOver={this.state.finishGame} handleCloseOverlay={this.handleCloseOverlay} finalText={this.state.finalText}/>
+          }
+        </div>
+        <div className={`${classes.gameCard} ${classes.inputPage}`}>
+          <h1 className={classes.welcome}>Welcome to Speed Game</h1>
+          <form onSubmit={this.handleInput}>
+              <div className={classes.nameInput}>
+                <label htmlFor="name">Nickname</label>
+                <input type="text" id="name"/>
+              </div>
+              <div className={classes.levelInput}>
+                <label htmlFor="level">Choose a level</label>
+                <select name="level" id="level" value="">
+                  <option value="" invalid="true" hidden>Choose a level...</option>
+                  <option value="Easy">Easy</option>
+                  <option value="Medium">Medium</option>
+                  <option value="Hard">Hard</option>
+                  <option value="Professional">Professional</option>
+                </select>
+              </div>
+            <input type="submit" />
+          </form>
         </div>
       </div>
+
     );
   }
 }
-
 export default App;
